b05ef9ed2bdc05abc40582843b43964b248e7076
Asif A Fasih <aafasih@gmail.com>
2021-09-08 15:57:11 -0500

    Global CircuitBreaker Configuration

---
+++ src/main/java/com/fun/simplereactiveapi/CircuitBreakerConfiguration.java
@@ -0,0 +1,34 @@
+package com.fun.simplereactiveapi;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerConfiguration {
+
+    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+            .failureRateThreshold(3)
+            .waitDurationInOpenState(Duration.ofMillis(50))
+            .slidingWindowSize(2)
+            .build();
+
+    TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+            .timeoutDuration(Duration.ofSeconds(4))
+            .build();
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> customConfiguration() {
+        return resilience4JCircuitBreakerFactory -> resilience4JCircuitBreakerFactory.configureDefault(id ->
+                new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(circuitBreakerConfig)
+                        .timeLimiterConfig(timeLimiterConfig)
+                        .build());
+    }
+}
